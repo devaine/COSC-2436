8cb96fe3da0b79b8bde9778624eeb799266cf612
devaine <devaine1203@gmail.com>
2024-10-11 12:57:18 +0000

    not finished yet

---
+++ lab/src/MusicLibrary.java
@@ -0,0 +1,6 @@
+/* MusicLibrary.java
+ * Purpose: Manages the collection of songs and implement sthe binary search algorithm.
+ * It will be used to demonstrate working with colleciton , file I/O, and algorithm implementation
+ */
+
+import com.google.gson.Gson;
